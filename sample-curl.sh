e25b4ea0a749c38843a43763d9b77a8530d5d2ed
tajuddin <tajuddin.n@sonata-software.com>
2024-02-20 18:22:41 +0530

    second commit

--- sample-curl.sh
+++ sample-curl.sh
@@ -8,20 +8,6 @@ function_url="https://<function-app-name>.azurewebsites.net/api/<function-key>"
 api_key="<function-key>"  ###"function-app-key (master)"
 set -x
 
-curl -m 59 -X POST "$function_url" -H "x-functions-key: $api_key" -H "Content-Type: application/json" -d '{"context": {"azure_subscription": "dev-sub","azure_location": "east us","client_id": "customer1","interaction_id": "test","execution_id": "id-1234"},"input_files": {"media":{"bucket_name": "247ai-stg-cca-customer1-staging","full_path": "2024/01/19/test/20240119035220_id-1234/test.wav","version": "0x8DC18A4BDF1C9CF"}},"staging_config": {"bucket_name": "247ai-stg-cca-customer1-staging","folder_path": "2024/01/19/test/20240119035220_id-1234","file_prefix": "test"},"function_config": {"signing_account": null}}'
-
-
-########################### OR ####################################
-#!/bin/bash
-
-# Replace with your Azure Function App URL
-###"https://<function-app-name>.azurewebsites.net/api/<function-key>"
-function_url="https://<function-app-name>.azurewebsites.net/api/<function-key>"
-
-# Replace with your Azure Function App key or any other authentication mechanism
-api_key="<function-key>"  ###"function-app-key (master)"
-set -x
-
 curl -m 59 -X POST "$function_url" \
 -H "x-functions-key: $api_key" \
 -H "Content-Type: application/json" \
@@ -33,6 +19,7 @@ curl -m 59 -X POST "$function_url" \
         "azure_subscription": "dev-sub",
         "azure_location": "east us",
         "client_id": "customer1",
+        "storageaccounturl": "https://<storage_account_name>.blob.core.windows.net",
         "interaction_id": "test",
         "execution_id": "id-1234"
     },
@@ -49,9 +36,6 @@ curl -m 59 -X POST "$function_url" \
         "file_prefix": "test"
     },
     "function_config": {
-        "signing_account": null
+        "signing_account": ""
     }
 }'
-
-
-
